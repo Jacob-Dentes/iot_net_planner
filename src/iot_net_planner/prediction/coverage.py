fcf85bda482a3a263da88d6d3d0c2602a27fe56c
Jacob Dentes <jmd477@cornell.edu>
2024-07-16 12:11:13 -0400

    Updated coverage

--- src/iot_net_planner/prediction/coverage.py
+++ src/iot_net_planner/prediction/coverage.py
@@ -4,9 +4,33 @@ Tools for predicting how much coverage a solution provides
 
 import numpy as np
 
-def get_coverage(sol, prr, min_weight=0.0):
+def get_coverages(sol, prr):
     """
-    Returns the coverage for building indices `sol` as preddicted by `prr`
+    Returns the coverage for building indices `sol` as predicted by `prr`
+
+    :param sol: an iterable of indices into prr indicating what to build
+
+    :param prr: a prr model initialized with demand points and facilities indexed in the same way as sol
+
+    :returns: a numpy array of the coverage for each demand point
+    """
+    coverages = [prr.get_prr(i) for i in sol]
+
+    # Compute the coverage at each demand point
+    dem_coverage = np.ones(len(coverages[0]))
+    for cov in coverages:
+        # The chance of failure is 1 - prr, so we multiply chances
+        # of failure to get chance they all fail
+        dem_coverage = dem_coverage * (1 - cov)
+    # The chance if success is 1 - chance of failure
+    dem_coverage = 1 - dem_coverage
+
+    return dem_coverage
+    
+
+def get_coverage_scalar(sol, prr, min_weight=0.0):
+    """
+    Returns the coverage for building indices `sol` as predicted by `prr`
 
     :param sol: an iterable of indices into prr indicating what to build
 
@@ -20,15 +44,6 @@ def get_coverage(sol, prr, min_weight=0.0):
     if len(sol) == 0:
         return 0.0
 
-    coverages = [prr.get_prr(i) for i in sol]
-
-    # Compute the coverage at each demand point
-    dem_coverage = np.ones(len(coverages[0]))
-    for cov in coverages:
-        # The chance of failure is 1 - prr, so we multiply chances
-        # of failure to get chance they all fail
-        dem_coverage = dem_coverage * (1 - cov)
-    # The chance if success is 1 - chance of failure
-    dem_coverage = 1 - dem_coverage
+    dem_coverage = get_coverages(sol, prr)
 
     return (1 - min_weight) * np.mean(dem_coverage) + (min_weight) * np.min(dem_coverage)
