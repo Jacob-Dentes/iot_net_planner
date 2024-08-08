9888db37ba52dc693c5a5cade80acf69c8f92759
Jacob Dentes <jmd477@cornell.edu>
2024-08-08 15:08:40 -0400

    Added a convenience script for filling dsms

---
+++ examples/fill_script.py
@@ -0,0 +1,16 @@
+from iot_net_planner.geo.dsm_filler import fill_file
+import sys
+
+def main(original, new, band=1):
+    fill_file(original, new)
+
+def __main__():
+    args = list(sys.argv[1:])
+
+    if len(args) == 3:
+        main(args[0], args[1], int(args[2]))
+    elif len(args) == 2:
+        main(args[0], args[1])
+    else:
+        print("Incorrect number of arguments. Expected old_dsm new_dsm")
+    
