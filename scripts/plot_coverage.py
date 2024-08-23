85258763d98837ef6e4fc47ac6d6d1a908863873
Jacob Dentes <jmd477@cornell.edu>
2024-08-23 12:44:26 -0400

    Added the option to write an image file instead

--- scripts/plot_coverage.py
+++ scripts/plot_coverage.py
@@ -9,7 +9,7 @@ from iot_net_planner.geo.plotting import plot_facs_coverage
 from iot_net_planner.prediction.coverage import get_coverages
 from iot_net_planner.prediction.prr_file import FileModel
 
-def main(dem_file, fac_file, prr_file, sol_file):
+def main(dem_file, fac_file, prr_file, sol_file, write_to_file=None):
     dems = gpd.read_file(dem_file).to_crs(epsg=4326)
     facs = gpd.read_file(fac_file).to_crs(dems.crs)
 
@@ -19,7 +19,7 @@ def main(dem_file, fac_file, prr_file, sol_file):
 
     prr = FileModel(dems, facs, model_file)
     coverages = get_coverages(sol, prr)
-    plot_facs_coverage(dems, facs, sol)    
+    plot_facs_coverage(dems, facs, sol, write_to_file)    
 
 if __name__ == "__main__":
     args = list(sys.argv[1:])
